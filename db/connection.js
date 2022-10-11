761bc547af0377149df95885fe922824e29e46a2
James Cañas <jamesricarocr@gmail.com>
2022-10-10 20:20:07 -0500

    ajustes despliegue

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,17 @@ const db = new Sequelize('piwdzivo_tenisCucuta', 'piwdzivo_ricardoCanas', 'contr
     dialect: 'mysql',
     // logging: false
     // esto resuelve el problema del nombre de las tablas
-    define:{freezeTableName:true}
+    define:{freezeTableName:true},
+    timezone: 'America/Bogota'
 });
+
+// const db = new Sequelize('piwdzivo_test', 'piwdzivo_ricardoCanas', 'controlremoto11', {
+//     host: '216.246.112.154',
+//     dialect: 'mysql',
+//     // logging: false
+//     // esto resuelve el problema del nombre de las tablas
+//     define:{freezeTableName:true},
+//     timezone: 'America/Bogota'
+// });
 
 module.exports = db;
